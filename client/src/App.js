ab5311513715689bb4f7c6f82eaa975027b90ba6
Maddie P <madelineprusak@gmail.com>
2019-04-29 21:08:26 -0400

    set up backend routes for login api, started working on authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  //function that runs when login form submitted
+  handleSubmit = e => { 
+    console.log("testing submit");
+    axios.post("/client/api/login").then(response => {
+      console.log(response);
+      // function that will run any errors
+    }).catch(error => {
+      console.log(error);
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -12,6 +23,19 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          hi.
+
+        {/* sign up form */}
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input type="email" placeholder="Email Address" name="email"></input>
+          <input type="password" placeholder="password" name="password"></input>
+        </form>
+        <button onClick={e => this.handleSubmit(e)}>Submit</button>
+        
+
+
+
+
         </p>
       </div>
     );
@@ -19,3 +43,4 @@ class App extends Component {
 }
 
 export default App;
+
