2fd98759c99bc9fb9f488b4d3eab9f7a38285082
Maddie P <madelineprusak@gmail.com>
2019-04-30 14:17:10 -0400

    got authentication working, need to add bootstrap/materialize cdns

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
-import React, { Component } from "react";
-import axios from "axios";
-import logo from "./logo.svg";
-import "./App.css";
 
+/* //app.js has all front end code. all logic goes about "html". nothing goes after render funciton.
 class App extends Component {
-  //function that runs when login form submitted
+  //function that runs when login form submitted. front end route to axios
   handleSubmit = e => { 
     console.log("testing submit");
     axios.post("/client/api/login").then(response => {
@@ -14,6 +11,7 @@ class App extends Component {
       console.log(error);
     });
   }
+  //render function contains "html"
   render() {
     return (
       <div className="App">
@@ -25,7 +23,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
           hi.
 
-        {/* sign up form */}
+        {/* sign up form /*}
         <form onSubmit={e => this.handleSubmit(e)}>
           <input type="email" placeholder="Email Address" name="email"></input>
           <input type="password" placeholder="password" name="password"></input>
@@ -41,6 +39,88 @@ class App extends Component {
     );
   }
 }
+*/
 
-export default App;
 
+
+
+import React from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+  Redirect,
+  Switch,
+	withRouter
+} from 'react-router-dom';
+import Auth from "./utils/Auth";
+import Nav from "./components/Nav";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import {Container} from "./components/Grid";
+import PublicRoute from "./pages/PublicRoute";
+import ProtectedRoute from "./pages/PublicRoute";
+import './App.css';
+
+//I want to add some basic inline styling here, even though we are bringing in styles
+const listStyle = {
+	color: 'cornflowerblue',
+	listStyle:'none'
+  };
+//Now we have all the stuff we need .. let's render some components with the Router
+const AuthExample = () => (
+	<Router>
+		<div>
+      		<Nav className="App-header"/>
+			<Container>
+				<AuthButton/>
+				<ul style={listStyle}>
+					<li><Link to="/public">Public Page</Link></li>
+					<li><Link to="/protected">Protected Page</Link></li>
+					<li><Link to="/register">Register a New User</Link></li>
+				</ul>
+				<Switch>
+					<Route path="/public" component={PublicRoute}/>
+					<Route path="/login" component={Login}/>
+					<Route path="/register" component={Register}/>
+					<PrivateRoute path="/protected" component={ProtectedRoute}/>
+					{/* <Route component={NoMatch} /> */}
+				</Switch>
+			</Container>
+		</div>
+	</Router>
+)
+
+
+//Authbutton component / withRouter is imported from react-router
+const AuthButton = withRouter(({ history }) => (
+	Auth.isAuthenticated ? (
+		<div className="container">
+			<p>Success! You are Logged In!</p>
+			<button className="btn btn-danger" 
+				onClick={() => {
+					Auth.signout(() => history.push('/'))
+				}}>
+				Sign out
+			</button>
+		</div>
+	) : (
+		<p>You are not logged in.</p>
+	)
+))
+
+// This is the private route component this checks for an authorized user here
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route {...rest} render={props => (
+		Auth.isAuthenticated ? (
+			<Component {...props}/>
+		) : (
+			<Redirect to={{
+				pathname: '/login',
+				state: { from: props.location }
+			}}/>
+		)
+	)}/>
+)
+
+export default AuthExample
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const apiRoutes = require("./api");
+
+//API Routes
+router.use("/api", apiRoutes);
+
+//If no API routes are hit, send the React app
+router.use((req, res) =>
+res.sendFile(path.join(__dirname, "../client/build/index.hmtl"))
+);
+
+// database calls
+
+module.exports = router;
+
+module.exports=router;
