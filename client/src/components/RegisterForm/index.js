1338e712f46c6ec8304dfe972fee7128b4690c6d
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-05-02 14:55:02 -0400

    form values of gender, age, type of account added with dropdown

--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -7,6 +7,9 @@ class RegisterForm extends React.Component {
 	form: null;
 	usernameElem: null;
 	passwordElem: null;
+	ageElem: null;
+	genderElem: null;
+
 
 	render() {
 		const { onRegister } = this.props;
@@ -20,13 +23,32 @@ class RegisterForm extends React.Component {
 						e.preventDefault();
 						return onRegister({
 							username: this.usernameElem.value,
-							password: this.passwordElem.value
+							password: this.passwordElem.value,
+							age: this.ageElem.value,
+							gender: this.genderElem.value,
+						
 						});
 					}}
 				>
 					<div className="form-group">
 						<input className="form-control" ref={(input) => this.usernameElem = input} type='text' name="username" placeholder='Enter Username' /><br/>
+						
 						<input className="form-control"  ref={(input) => this.passwordElem = input} type='password' name="password" placeholder='Password' /><br/>
+						
+						<input className="form-control"  ref={(input) => this.ageElem = input} type='integer' name="age" 
+						placeholder='Age' /><br/>
+						
+						<input className="form-control"  ref={(input) => this.genderElem = input} type='text' name="gender" placeholder='Gender' /><br/>
+						
+						<div className="dropdown">
+  						<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    					Dropdown button
+  						</button>
+  						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    					<a class="dropdown-item" href="#">Admin</a>
+    					<a class="dropdown-item" href="#">User</a>
+ 					 	</div>
+						</div>
 						<button className="btn btn btn-primary" type='submit'>Submit</button>
 					</div>
 				</form>
