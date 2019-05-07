cd05e62bff1440076968d7fdf6f11581a1f1c55f
Guest <User@Vanessas-MacBook-Air.local>
2019-05-07 14:07:18 -0400

    created submission controller and all submit api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./users");
-
+const submitRoutes = require("./allsubmit");
 //User Routes
 router.use("/users", userRoutes);
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+//question submissions
+router.use("/submit", submitRoutes);
 // For anything else, render the html page
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
