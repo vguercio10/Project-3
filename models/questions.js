e6b153075c7d9b3dada3d99781a3ea5cb588e035
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-05-07 12:32:46 -0400

    user question schema created

---
+++ models/questions.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const Question = new Schema({
+    username: String,
+    question: String,
+    answer: Boolean,
+    response: String,
+    respondantsName: String
+});
+
+Account.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('questions', Question);
