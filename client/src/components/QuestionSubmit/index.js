54cfdb263f7830c1ac81967c02fb4d6c9f78096c
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-05-07 15:16:53 -0400

    question submit component started

--- client/src/components/QuestionSubmit/index.js
+++ client/src/components/QuestionSubmit/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
-import QuestionsForm from "../RegisterForm";
+import QuestionsForm from '../QuestionsForm';
 //the component for submitting a question to the mom dashboard
 class QuestionSubmit extends React.Component {
+	//Setting components initial state
 	state = {
-		redirectToReferrer: false
+		question: "",
+		answer: "",
+		response: "",
+		respondantsName: ""
 	}
+	handleInputChange = event => {
+		// Getting the value and name of the input which triggered the change
+		const { question, answer, response, respondantsName } = event.target;
+	
+		// Updating the input's state
+		this.setState({
+		  [question]: value,
+		  [answer]: value,
+		  [response]: value,
+		  [respondantsName]: value
+		});
+	  };
+
+	handleFormSubmit = event => {
+
+		event.preventDefault();
+
+		fetch('/api/submit/questionsave', {
+			method: 'POST',
+			body: JSON.stringify(data),
+		}).then((response) => {
+			if (response.status === 200) {
+				console.log('Succesfully registered user!');
+			}
+		})
+		.catch((err) => {
+			console.log('Error registering user.', err);
+		});
+	}
+
+
+
+}
+export default QuestionSubmit
